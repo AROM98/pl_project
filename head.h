97835d65aa00c9a6cc0660364757ddeb62f2cf9f
AROM98 <rafa.olim@sapo.pt>
2020-03-16 00:14:58 +0000

    apanha email

---
+++ head.h
@@ -0,0 +1,13 @@
+#ifndef _HEAD
+#define _ HEAD 1
+
+//estas definiçoes aqui é um exemplo de como podemos usar
+#define OP 100
+#define AP 101
+#define FP 102
+#define INT 103
+#define ERRO 104
+
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,14 @@
+#include "head.h"
+#include <stdio.h>
+#include <string.h>
+
+//criar var global para o nome do projecto?
+
+extern int yylex();
+
+int main (int argc, char* argv[]){
+    //no qual argv[1] será o nome do projecto a criar
+    // e argv[2] será o nome do template (com extensao)
+    
+    return 0;
+}
