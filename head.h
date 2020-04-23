0f272e00a8fdc2d7a33bb6bf51c48a2cefcd213b
AROM98 <rafa.olim@sapo.pt>
2020-04-23 13:50:20 +0100

    Revert "Revert "final version""
    This reverts commit 116b077985a1f26485d260893b9c837cfeb0b678.

--- head.h
+++ head.h
@@ -11,7 +11,7 @@ char* dir; //dir tmp
 
 
 //para a tree
-typedef char* tree[50];
+typedef char* Array[50];
 
 typedef GHashTable* tabela;
 
@@ -21,7 +21,7 @@ char* nomefich;
 char* extensao;
 
 int contatracos(char* string);
-void putinTree(tree t, char* string, int i);
+void guardapath(Array t, char* string, int i);
 void createfich(char* nome, char* path);
 
 char* STRCAT (char* dest, const char* src);
